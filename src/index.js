cdd1e42b5ebe965c88604d92c09079e91c469ba4
Javi Gomez <rtomyj@gmail.com>
2019-10-13 11:54:27 -0500

    Removed defaults

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ListS3Resources from './ListS3Resources';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ListS3Resources />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
